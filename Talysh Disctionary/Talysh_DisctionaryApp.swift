d3efe68a1e13e9c532d3a1a4a94ffc3626c3a659
kaizer1 <nitche9@gmail.com>
2021-03-29 08:37:56 +0300

    Initial Commit

---
+++ Talysh Disctionary/Talysh_DisctionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Talysh_DisctionaryApp.swift
+//  Talysh Disctionary
+//
+//  Created by Сергей Лоскутников on 29.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Talysh_DisctionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
